9edb42955684535bef8eae715e9dd9d90f9b23cb
SAJID LATIF <indus1530@gmail.com>
2021-06-24 16:23:45 +0500

    MainApp URL changed SHRUC_HC to shrc_camps

--- app/src/main/java/edu/aku/hassannaqvi/shrc_camps/core/MainApp.java
+++ app/src/main/java/edu/aku/hassannaqvi/shrc_camps/core/MainApp.java
@@ -26,7 +26,7 @@ public class MainApp extends Application {
     public static final String _IP = "https://vcoe1.aku.edu";// .LIVE server
     //public static final String _IP = "http://f38158";// .TEST server
     //public static final String _IP = "http://43.245.131.159:8080";// .TEST server
-    public static final String _HOST_URL = MainApp._IP + "/SHRUC_HC/api/";// .TEST server;
+    public static final String _HOST_URL = MainApp._IP + "/shrc_camps/api/";// .TEST server;
     public static final String _SERVER_URL = "sync.php";
     public static final String _SERVER_GET_URL = "getData.php";
     public static final String _PHOTO_UPLOAD_URL = _HOST_URL + "uploads.php";
